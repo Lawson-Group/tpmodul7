2fdb378e8c63a645b76df247611b2bdba5c97748
Hasnannn <jannatinnurrohmah@gmail.com>
2023-04-02 22:10:26 +0700

    Update my Program.cs

--- tpmodul7_kelompok_2/Program.cs
+++ tpmodul7_kelompok_2/Program.cs
@@ -1,7 +1,8 @@
 using tpmodul7_kelompok_2;
 
+DataMahasiswa1302213051 dm = new DataMahasiswa1302213051();
+dm.ReadJSON();
+
 KuliahMahasiswa1302213051 km = new KuliahMahasiswa1302213051();
 km.ReadJSON();
 
-DataMahasiswa1302213051 dm = new DataMahasiswa1302213051();
-dm.ReadJSON();
