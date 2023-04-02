98ec80f8d896f36ff26f29a1befa0190c552d4cd
novitangrh <novitasabila70@gmail.com>
2023-04-02 21:24:29 +0700

    menambahkan class KuliahMahasiswa_1302213030_Novita

--- tpmodul7_kelompok_2/Program.cs
+++ tpmodul7_kelompok_2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul7_kelompok_2;
+
+KuliahMahasiswa_1302213030_Novita km = new KuliahMahasiswa_1302213030_Novita();
+km.ReadJSON();
