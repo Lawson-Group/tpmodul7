cf26a20670ab6c9f6a352e495b1d4dbc8ba8ce6d
Hasnannn <jannatinnurrohmah@gmail.com>
2023-04-02 21:27:18 +0700

    Menambahkan class KuliahMahasiswa1302213051

--- tpmodul7_kelompok_2/Program.cs
+++ tpmodul7_kelompok_2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul7_kelompok_2;
+
+KuliahMahasiswa1302213051 km = new KuliahMahasiswa1302213051();
+km.ReadJSON();
