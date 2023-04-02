67aa693bb9d0aa93f0228a02baf2ce3562e87ecd
muthiakhrnsa3 <127019755+muthiakhrnsa3@users.noreply.github.com>
2023-04-02 23:37:08 +0700

    commit

---
+++ tpmodul7_kelompok_2/Program.cs
@@ -0,0 +1,13 @@
+﻿using tpmodul7_kelompok_2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Datamahasiswa_1302213062 Alvian = new Datamahasiswa_1302213062();
+        Alvian.ReadJSON();
+
+        KuliahMahasiswa_1302213062 Supriadi = new KuliahMahasiswa_1302213062();
+        Supriadi.ReadJSON();
+    }
+}
