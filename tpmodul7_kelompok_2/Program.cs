19a065b30a7a1109096507039b209a3c1b8f70bb
novitangrh <novitasabila70@gmail.com>
2023-04-02 20:28:21 +0700

    menambahkan class DataMahasiswa_1302213030_Novita

--- tpmodul7_kelompok_2/Program.cs
+++ tpmodul7_kelompok_2/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul7_kelompok_2;
+
+DataMahasiswa_1302213030_Novita dm = new DataMahasiswa_1302213030_Novita();
+dm.ReadJSON();
